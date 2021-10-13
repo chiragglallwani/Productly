b9d814fb247ef0feb9c7e98fc90a2af294996925
lalwanichirag3 <57692146+lalwanichirag3@users.noreply.github.com>
2021-10-13 14:08:36 -0700

    debugging cors

--- src/API/axios.js
+++ src/API/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5001/productly-80dfc/us-central1/api',
+    baseURL: 'https://localhost:5001/productly-80dfc/us-central1/api',
 });
 
 export default instance;
