166e2e1c967e66873fda309e0ab18d17fa49d141
Chirag Lalwani <chiraglalwani@Chirags-MBP.lan>
2023-06-08 17:36:20 -0700

    switch api to production

--- src/API/axios.js
+++ src/API/axios.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_FIREBASE_FUNCTIONS_API_KEY_LOCAL,//'https://us-central1-productly-61da0.cloudfunctions.net/api'//'http://localhost:5001/productly-80dfc/us-central1/api',
+  baseURL: process.env.REACT_APP_FIREBASE_FUNCTIONS_API_PRODUCTION, //'https://us-central1-productly-61da0.cloudfunctions.net/api'//'http://localhost:5001/productly-80dfc/us-central1/api',
 });
 
 export default api;
